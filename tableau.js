f09533699720a249242f701538204b04f450f830
raju <rajukumar.rjk@gmail.com>
2021-01-17 21:04:30 +0530

    added filter and change view option to the page

--- tableau.js
+++ tableau.js
@@ -1,8 +1,40 @@
 
-function initViz() {
-    var containerDiv = document.getElementById("vizContainer"),
-        url = "https://public.tableau.com/views/Loan_16107817213780/Story1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
+var viz;
 
-    var viz = new tableau.Viz(containerDiv, url);
+window.onload= function() {
+
+    var vizDiv = document.getElementById('dashboard');
+    var vizURL = 'https://public.tableau.com/views/DashboardtotestwithTableauJavaScriptAPI/allloans?:language=en&:display_count=y&:origin=viz_share_link';
+    var options = {
+        width: '1000px',
+        height: '900px',
+        hideToolbar: true,
+        hideTabs: true
+    };
+    viz = new tableau.Viz(vizDiv, vizURL, options);
+};
+
+function loadView(sheetName) {
+    var workbook = viz.getWorkbook();
+    workbook.activateSheetAsync(sheetName);
 }
 
+
+function show(filterName, values) {
+    console.log(filterName, values)
+    var sheet = viz.getWorkbook().getActiveSheet();
+    sheet.applyFilterAsync(filterName, values, tableau.FilterUpdateType.REPLACE);
+}
+
+
+$('.option').click(function (){
+    $('.option').removeClass('active');
+    $(this).addClass('active');
+})
+
+$(document).ready(function (){
+    $(".hint").popover({
+        placement: "right",
+        trigger: 'hover'
+    });
+})
